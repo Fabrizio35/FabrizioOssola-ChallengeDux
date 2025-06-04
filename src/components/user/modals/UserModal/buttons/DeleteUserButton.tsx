82203d384fdeaa66b9269ba889456fa8d6b9abcf
Fabrizio Ossola <ossolafabrizio@gmail.com>
2025-06-04 11:17:48 -0300

    add ConfirmDialog in DeleteUserButton

--- src/components/user/modals/UserModal/buttons/DeleteUserButton.tsx
+++ src/components/user/modals/UserModal/buttons/DeleteUserButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'primereact/button'
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'
 
 interface DeleteUserButtonProps {
   handleDelete: () => void
@@ -9,18 +10,49 @@ const DeleteUserButton = ({
   handleDelete,
   isSubmitting,
 }: DeleteUserButtonProps) => {
+  const HeaderConfirm = () => (
+    <h3
+      className="text-xl font-bold text-white p-3 m-0 mb-5"
+      style={{ backgroundColor: '#0763E7' }}
+    >
+      Confirmar eliminación
+    </h3>
+  )
+
+  const MessageConfirm = () => (
+    <span className="font-semibold text-lg">
+      ¿Estás seguro de que deseas eliminar este usuario?
+    </span>
+  )
+
+  const showConfirm = () => {
+    confirmDialog({
+      header: <HeaderConfirm />,
+      message: <MessageConfirm />,
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Eliminar',
+      rejectLabel: 'Cancelar',
+      acceptClassName: 'p-button-danger',
+      accept: handleDelete,
+      closable: false,
+    })
+  }
+
   return (
-    <Button
-      type="button"
-      icon="pi pi-trash"
-      severity="danger"
-      tooltip="Eliminar usuario"
-      tooltipOptions={{ position: 'bottom', className: 'text-xs' }}
-      className="p-button-text text-white"
-      style={{ background: 'transparent', border: 'none' }}
-      onClick={handleDelete}
-      disabled={isSubmitting}
-    />
+    <>
+      <Button
+        type="button"
+        icon="pi pi-trash"
+        severity="danger"
+        tooltip="Eliminar usuario"
+        tooltipOptions={{ position: 'bottom', className: 'text-xs' }}
+        className="p-button-text text-white"
+        style={{ background: 'transparent', border: 'none' }}
+        onClick={showConfirm}
+        disabled={isSubmitting}
+      />
+      <ConfirmDialog />
+    </>
   )
 }
 
