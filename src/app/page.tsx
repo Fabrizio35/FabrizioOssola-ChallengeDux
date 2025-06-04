b67a76c69d41e6e5e169f20d0b89a65274474275
Fabrizio Ossola <ossolafabrizio@gmail.com>
2025-06-04 00:11:25 -0300

    add SuspenseLoader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 import { Suspense } from 'react'
 import UserDashboardSSRWrapper from '@/components/user/dashboard/UserDashboardSSRWrapper'
+import SuspenseLoader from '@/components/SuspenseLoader'
 
 export default function Home() {
   return (
     <main className="px-5 pt-6 ml-7 min-h-screen">
-      <Suspense
-        fallback={
-          <div
-            style={{ color: '#0763E7' }}
-            className="mt-5 ml-5 text-xl font-bold"
-          >
-            Cargando...
-          </div>
-        }
-      >
+      <Suspense fallback={<SuspenseLoader />}>
         <UserDashboardSSRWrapper sector="1111" />
       </Suspense>
     </main>
---
+++ src/components/SuspenseLoader.tsx
@@ -0,0 +1,9 @@
+const SuspenseLoader = () => {
+  return (
+    <div style={{ color: '#0763E7' }} className="mt-5 ml-5 text-xl font-bold">
+      Cargando...
+    </div>
+  )
+}
+
+export default SuspenseLoader
